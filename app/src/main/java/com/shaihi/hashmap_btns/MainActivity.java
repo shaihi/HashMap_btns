c71daa557482af48df0d243610d44a7a40989dcf
shaihi <shaihi@icloud.com>
2025-01-07 22:34:35 +0200

    Added all necessary finctionality

--- app/src/main/java/com/shaihi/hashmap_btns/MainActivity.java
+++ app/src/main/java/com/shaihi/hashmap_btns/MainActivity.java
@@ -1,6 +1,11 @@
 package com.shaihi.hashmap_btns;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText etKey, etValue;
+    Button btnAdd, btnClear;
+    LinearLayout llButtons;
+    HashMap<String,String> dictionary = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +31,47 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        etKey = findViewById(R.id.etKey);
+        etValue = findViewById(R.id.etValue);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnClear = findViewById(R.id.btnClear);
+        llButtons = findViewById(R.id.llButtons);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = etKey.getText().toString();
+                String value = etValue.getText().toString();
+                dictionary.put(key,value);
+                Button tmpBtn = new Button(MainActivity.this);
+                tmpBtn.setText(key);
+                tmpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                llButtons.addView(tmpBtn);
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llButtons.removeAllViews();
+                ArrayList<String> keys = new ArrayList<>(dictionary.keySet());
+                keys.sort(String::compareTo);
+                for(String keyItem: keys){
+                    Button tmpBtn = new Button(MainActivity.this);
+                    tmpBtn.setText(keyItem);
+                    tmpBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(MainActivity.this, dictionary.get(keyItem), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    llButtons.addView(tmpBtn);
+                }
+            }
         });
     }
 }
